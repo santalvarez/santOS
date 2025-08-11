46d2410b1a30e53eab77b840a98b3fe532ce58b8
santalvarez_meli <santiago.salvarez@mercadolibre.com>
2025-08-11 16:40:40 -0300

    Remove mmio lib

--- Package.swift
+++ Package.swift
@@ -7,13 +7,10 @@ let package = Package(
     products: [
         .executable(name: "santOS", targets: ["Kern"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-mmio", branch: "main")
-    ],
     targets: [
         .executableTarget(
             name: "Kern",
-            dependencies: ["ArmV7", .product(name: "MMIO", package: "swift-mmio")],
+            dependencies: ["ArmV7"],
             path: "Sources/Kern"
         ),
         .target(
